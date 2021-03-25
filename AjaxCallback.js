72605cf7b687efea276bec28271b776e7a918749
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-03-25 12:02:58 +0530

    ADDED: ajaxcallback and promises file

---
+++ AjaxCallback.js
@@ -0,0 +1,60 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+// function showTime() {
+//   const date = new Date();
+//   return (
+//     date.getHours() +
+//     "HRS:" +
+//     date.getMinutes() +
+//     "Mins:" +
+//     date.getSeconds() +
+//     "secs"
+//   );
+// }
+
+function makeAJAXCall(methodType, url, callback, async = true, data) {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    console.log(
+      methodType +
+        " State changed Called at ready state :" +
+        xhr.readyState +
+        " Status: " +
+        xhr.status
+    );
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200 || xhr.status === 201) {
+        callback(xhr.responseText);
+      } else if (xhr.status >= 400) {
+        console.log("handle 400 client error 500 server error");
+      }
+    }
+  };
+  xhr.open(methodType, url, async);
+  if (data) {
+    console.log(JSON.stringify(data));
+    xhr.setRequestHeader("Content-Type", "applicaton/json");
+    console.log(xhr);
+    xhr.send(JSON.stringify(data));
+  } else xhr.send();
+  console.log(methodType + "request sent to the server at:");
+}
+
+// const getURL = " http://localhost:3000/employee";
+// function getUserDetails(data) {
+//   console.log("Get User Data:" + data);
+// }
+// makeAJAXCall("GET", getURL, getUserDetails, true);
+
+// const deleteURL = "http://localhost:3000/employee/4";
+// function userDeleted(data) {
+//   console.log("USer deleted" + data);
+// }
+// makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+
+const postURL = "http://localhost:3000/employee";
+const emplData = { "name": "Anju", "department": "Sales" };
+function userAdded(data) {
+  console.log("User Added" + data);
+}
+makeAJAXCall("POST", postURL, userAdded, false, emplData);
